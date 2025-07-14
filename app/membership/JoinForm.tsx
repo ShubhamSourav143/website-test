6cdb23194eb8254e7dd5644031f278133acb878e
ShubhamSourav143 <68903423+ShubhamSourav143@users.noreply.github.com>
2025-07-14 20:24:44 +0530

    Update JoinForm.tsx

--- app/membership/JoinForm.tsx
+++ app/membership/JoinForm.tsx
@@ -1,1 +1,12 @@
+// components/membership/JoinForm.tsx
 
+import React from 'react';
+
+export default function JoinForm() {
+  // We'll add state, handlers, and UI in Bolt next!
+  return (
+    <form>
+      {/* Membership form fields and logic go here */}
+    </form>
+  );
+}
